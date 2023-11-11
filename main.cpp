16c3385a7e2470a171cabab327d9844fd59c0120
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2023-11-11 13:58:07 +0300

    fixes

--- main.cpp
+++ main.cpp
@@ -11,27 +11,6 @@ int main() {
     LevelInitializer levelInitializer;
     levelInitializer.handler();
 
-//    Message::message("Game Start State");
-//    board.printBoard();
-//
-//    cout << "<----------------------------------->" << endl;
-//
-//    auto start_time = chrono::high_resolution_clock::now();
-//    Algorithm::bfs(board);
-//    auto end_time = chrono::high_resolution_clock::now();
-//
-//    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-//
-//    cout << "-------------------------------------------------------------\n";
-//    cout << "Execution Time In Micro Seconds Ss : " << duration.count() << " Micro Second" << endl;
-//    cout << "-------------------------------------------------------------\n";
-
-//    board.printBoard();
-
-//    for (int i = 0; i < board.allowedMoves; i++) {
-//        repel = board.moveAttract(*repel);
-//    }
-
     return 0;
 }
 
